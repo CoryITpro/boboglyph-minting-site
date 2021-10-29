c55e3b036c5ca931f909850f2c4e79fcc6818e99
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-30 01:02:55 +0900

    member created

---
+++ src/components/Membership/index.js
@@ -0,0 +1,10 @@
+import "./style.scss"
+
+const Membership = () => {
+  return (
+    <div className="member">
+      <div className="member-container"></div>
+    </div>
+  )
+}
+export default Membership
