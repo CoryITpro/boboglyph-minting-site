998e3b998b9d3d2222837bc652ea4219f974426f
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-30 01:31:15 +0900

    green done

--- src/components/Membership/index.js
+++ src/components/Membership/index.js
@@ -1,9 +1,42 @@
 import "./style.scss"
-
+import { Bobo5 } from "resources"
 const Membership = () => {
   return (
     <div className="member">
-      <div className="member-container"></div>
+      <div className="member-container">
+        <div className="member-left-container">
+          <div className="brown-heading">
+            <h3>
+              The Bobo-Glyphs draw from an ancient power to determine where the wealth is going. Use the past to find the future.
+            </h3>
+          </div>
+          <div className="member-left-down-container">
+            <p>
+              Holders who mint at least one Bobo-Glyph NFT in their wallet will become Association members. Members gain premier / early access to new collections.
+            </p>
+            <p>
+              Association membership also entitles you to:
+            </p>
+            <ul>
+              <li aria-level="1"><b>Vote on the next collection and future of the project.</b></li>
+            </ul>
+            <ul>
+              <li aria-level="1"><b>Be a member on our board of trustees.</b></li>
+            </ul>
+            <ul>
+              <li aria-level="1"><b>Vote and bid on OWNING AAoC episodes.</b></li>
+            </ul>
+            <ul>
+              <li aria-level="1"><b>Members-only Q&amp;A and so much more.</b></li>
+            </ul>
+          </div>
+        </div>
+        <div className="member-right-container">
+          <div className="img-well">
+            <img src={Bobo5} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
