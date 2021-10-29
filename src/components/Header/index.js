2252d4cd0789fb98839a711f7b0af42b98d2189c
jeus0430 <jeus0430.tazan@gmail.com>
2021-10-30 00:31:38 +0900

    header finished

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,78 @@
 import "./style.scss"
-import { RedAAoC } from "../../resources"
+import { RedAAoC, Bobo1, Bobo2, Bobo3, Bobo4, Bobo5, Bobo6, Bobo7, Bobo8, Bobo9 } from "../../resources"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 
 const Header = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 1000,
+    autoPlay: true
+  }
 
   return (
     <div className="header">
       <div className="header-container">
-        <div className="header-up-img-continaer">
+        <div className="header-up-img-container">
           <img src={RedAAoC} alt="red-img" className="header-red-img" />
+        </div>
+        <div className="header-down-img-container">
+          <div className="header-slick-container">
+            <Slider {...settings}>
+              <div>
+                <img src={Bobo1} />
+              </div>
+              <div>
+                <img src={Bobo2} />
+              </div>
+              <div>
+                <img src={Bobo3} />
+              </div>
+              <div>
+                <img src={Bobo4} />
+              </div>
+              <div>
+                <img src={Bobo5} />
+              </div>
+              <div>
+                <img src={Bobo6} />
+              </div>
+              <div>
+                <img src={Bobo7} />
+              </div>
+              <div>
+                <img src={Bobo8} />
+              </div>
+              <div>
+                <img src={Bobo9} />
+              </div>
+            </Slider>
+          </div>
+          <div className="header-quote-container">
+            <h1>
+              These are REAL (digital) artifacts.
+            </h1>
+            <h3>
+              I don’t know how many times I gotta keep telling you, but this is it.&nbsp;
+            </h3>
+            <p>
+              The AAoC is dedicated to finding and minting the RAREST and most valuable digital artifacts on the Web.&nbsp;
+            </p>
+            <p>
+              This is a super limited, <em>exclusive</em> collection of <em><strong>only</strong></em> <strong>1125</strong> Bobo-Glyph NFTs.
+              Each one is full of authentic digital imperfections, erosion, and centuries of internet update damage.&nbsp;&nbsp;
+            </p>
+            <p>
+              These are NOT another generative animal cash grab.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/resources/index.js
+++ src/resources/index.js
@@ -7,6 +7,15 @@ import HeroBanner from "./hero-banner.png"
 import GalleryBanner from "./gallery-banner.png"
 import CircleSwish from "./circle-swish.png"
 import RoadmapStep from "./roadmap-step.png"
+import Bobo1 from "./bobo1.png"
+import Bobo2 from "./bobo2.png"
+import Bobo3 from "./bobo3.png"
+import Bobo4 from "./bobo4.png"
+import Bobo5 from "./bobo5.png"
+import Bobo6 from "./bobo6.png"
+import Bobo7 from "./bobo7.png"
+import Bobo8 from "./bobo8.png"
+import Bobo9 from "./bobo9.png"
 
 export {
   RedAAoC,
@@ -18,4 +27,13 @@ export {
   GalleryBanner,
   CircleSwish,
   RoadmapStep,
+  Bobo1,
+  Bobo2,
+  Bobo3,
+  Bobo4,
+  Bobo5,
+  Bobo6,
+  Bobo7,
+  Bobo8,
+  Bobo9
 }
