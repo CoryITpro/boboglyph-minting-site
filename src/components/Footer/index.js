45a20b16f2c43a077d3b8d6d964fbac6d18a334f
Cory <coryitpro@gmail.com>
2021-11-08 21:47:16 -0500

    Add social links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,4 @@
+import { FaTwitter, FaDiscord } from "react-icons/fa"
 import { RedAAoC } from "resources"
 import "./style.scss"
 
@@ -5,6 +6,24 @@ const Footer = () => (
   <div className="footer flex">
     <div className="footer-wrapper container flex flex-column">
       <img src={RedAAoC} alt="red" loading="lazy" />
+      <div className="footer-socials flex">
+        <a
+          className="flex"
+          href="https://twitter.com/archaelogyassoc"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          className="flex"
+          href="https://discord.gg/sXjENcZm54"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaDiscord />
+        </a>
+      </div>
       <p>Home of the Authenticated BOBOGLYPH</p>
     </div>
   </div>
